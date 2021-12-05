b45b614b9b14a2229573d91da857d9d072b1ee91
Adrian Trevino <42710869+adtrevino@users.noreply.github.com>
2021-12-05 12:47:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
+// FUNCTION FOR COMPUTING THE SIMPLE INTEREST
 function compute()
 {
-    p = document.getElementById("principal").value;
+    // CREATING AND ASSIGNING VARIABLES
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var text = "\<br\>If you deposit "+"<mark>"+principal+"</mark>"+",\
+                <br\>at an interest rate of "+"<mark>"+rate+"%"+"</mark>"+"\
+                <br\>you will receive an amount of "+"<mark>"+amount+"</mark>"+",\
+                <br\>in the year "+"<mark>"+year+"</mark>"+"\<br\>"
     
+    // CHECK IF THE USER DID NOT GIVE AN INPUT
+    if (principal == ""){
+        alert("Please enter an amount");
+        principal.focus();
+        return false;
+    }
+
+    // IF INPUT WAS GIVEN, CHECK IF IT IS LESS THAN OR EQUAL TO ZERO
+    if (principal <= 0){
+        alert("Please enter a positive number");
+        principal.focus();
+        return false;
+    }
+
+    // IF ALL IS CORRECT, THEN PROCEED WITH GIVING THE FINAL RESULTS
+    document.getElementById("result").innerHTML=text
 }
-        
+
+// FUNCTION FOR DYNAMICALLY UPDATING THE RATE OF INTEREST 
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    
+    document.getElementById("rate_val").innerText=rateval;
+}
